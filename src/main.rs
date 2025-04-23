c2e2337580b155473f5ec7ff507dd0ba383619db
Marc-Jr <atebemarcjr@icloud.com>
2025-04-23 08:17:05 +0100

    fix: allow parallel task start and print elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use mini_async_runtime::{MiniRuntime, TimerFuture};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 async fn sleep(duration: Duration) {
     TimerFuture::new(duration).await;
@@ -7,20 +7,27 @@ async fn sleep(duration: Duration) {
 
 async fn task_one() {
     println!("task one: start");
+    let start = Instant::now();
     sleep(Duration::from_secs(1)).await;
-    println!("task one: done");
+    let elapsed = start.elapsed().as_secs();
+    println!("task one: done   [~{}s]", elapsed);
 }
 
 async fn task_two() {
     println!("task two: start");
+    let start = Instant::now();
     sleep(Duration::from_secs(2)).await;
-    println!("task two: done");
+    let elapsed = start.elapsed().as_secs();
+    println!("task two: done   [~{}s]", elapsed);
 }
 
 fn main() {
     let mut rt = MiniRuntime::new();
     println!("Runtime started...");
+
     rt.spawn(task_one());
     rt.spawn(task_two());
-    rt.block_on(async {}); // Kick off polling
+
+    // Just run an empty future to start the runtime loop
+    rt.block_on(async {});
 }
